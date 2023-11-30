c76cbe68d79a79395c801a7127ded16078db93b8
Nick <darkforceyoda@gmail.com>
2023-11-30 17:09:43 -0600

    slayed the horde of zombie code

--- components/services/Card.tsx
+++ components/services/Card.tsx
@@ -9,7 +9,6 @@ type CardProps = {
 
 const Card: React.FC<CardProps> = ({ title, icon, description }) => {
   return (
-    // <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
     <div className="rounded-lg hover:bg-black hover:shadow-lg px-4 py-8 h-[240px] ease-out duration-700">
       <div className="flex items-center gap-4 py-2">
         <div className="bg-blue-400 rounded-lg p-2">{icon}</div>
@@ -17,7 +16,6 @@ const Card: React.FC<CardProps> = ({ title, icon, description }) => {
       </div>
       <p>{description}</p>
     </div>
-    // </motion.div>
   );
 };
 
