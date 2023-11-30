1687eee7b917af1ba09f4c8aef08d1c9d42ff3ac
Nick <darkforceyoda@gmail.com>
2023-11-30 11:15:33 -0600

    framer motion

--- components/testimonials/Testimonials.tsx
+++ components/testimonials/Testimonials.tsx
@@ -1,36 +1,50 @@
+"use client";
 import React from "react";
 import Card from "./Card";
 import profileImage from "../../public/profile.png";
 import mary from "../../public/mary-kleine.jpg";
+import { motion } from "framer-motion";
 
 type TestimonialsProps = {};
 
 const Testimonials: React.FC<TestimonialsProps> = () => {
   return (
     <section className="w-full min-h-screen">
-      <h2 className="text-center text-3xl md:text-6xl font-bold py-8">
-        Testimonials
-      </h2>
-      <div className="grid lg:grid-cols-3 py-4 gap-4">
-        <Card
-          name="Mary"
-          link="thistleandthornsllc.com"
-          image={mary}
-          content="lorem ipsadjf sdjfbkasjd flasdijfoiasesdfasdn fffffffffffffffffff fffffffaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaafffffnf lsajdfhn c a,sdfhoasild fjsfdh"
-        />
-        <Card
-          name="Mary"
-          link="thistleandthornsllc.com"
-          image={profileImage}
-          content="lorem ipsadjf sdjfbkasjd flasdijfoiasenf lsajdfhn ca,sdfhoasild fjsfdh"
-        />
-        <Card
-          name="Mary"
-          link="thistleandthornsllc.com"
-          image={profileImage}
-          content="lorem ipsadjf sdjfbkasjd flasdijfoiasenf lsajdfhn ca,sdfhoasild fjsfdh"
-        />
-      </div>
+      <motion.div
+        initial={{ y: 200, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <h2 className="text-center text-3xl md:text-6xl font-bold py-8">
+          Testimonials
+        </h2>
+      </motion.div>
+      <motion.div
+        initial={{ y: 200, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <div className="grid lg:grid-cols-3 py-4 gap-4">
+          <Card
+            name="Mary"
+            link="thistleandthornsllc.com"
+            image={mary}
+            content="lorem ipsadjf sdjfbkasjd flasdijfoiasesdfasdn fffffffffffffffffff fffffffaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaafffffnf lsajdfhn c a,sdfhoasild fjsfdh"
+          />
+          <Card
+            name="Mary"
+            link="thistleandthornsllc.com"
+            image={profileImage}
+            content="lorem ipsadjf sdjfbkasjd flasdijfoiasenf lsajdfhn ca,sdfhoasild fjsfdh"
+          />
+          <Card
+            name="Mary"
+            link="thistleandthornsllc.com"
+            image={profileImage}
+            content="lorem ipsadjf sdjfbkasjd flasdijfoiasenf lsajdfhn ca,sdfhoasild fjsfdh"
+          />
+        </div>
+      </motion.div>
     </section>
   );
 };
