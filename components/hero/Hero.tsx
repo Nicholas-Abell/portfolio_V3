bab2cad9a05361c662919cc0d60462a5ecf69ad6
Nick <darkforceyoda@gmail.com>
2023-12-01 22:46:55 -0600

    button styles

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -8,7 +8,8 @@ type HeroProps = {};
 
 const Hero: React.FC<HeroProps> = () => {
   return (
-    <section className="w-full min-h-screen flex flex-col md:flex-row justify-center items-center gap-8 mx-auto px-2.5 md:px-20">
+    <section className="w-full min-h-screen grid md:grid-cols-2 items-center gap-8 mx-auto px-2.5 md:px-48 pt-8 relative overflow-hidden">
+      <div className="bg-[#f5eef0] w-[1600px] h-[900px] absolute bottom-60 right-[-360px] rotate-45" />
       <motion.div
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
@@ -21,8 +22,12 @@ const Hero: React.FC<HeroProps> = () => {
           I am passionate about solving bussiness problems through innovation
         </p>
         <div className="grid grid-cols-2 items-center gap-4 pr-24">
-          <button className="bg-blue-800 p-4 rounded-lg">See My Work</button>
-          <button className="bg-blue-800 p-4 rounded-lg">Contact ME</button>
+          <button className="bg-[#f1cbd6] hover:bg-[#f5eef0] ease-out duration-200 p-4 rounded-lg font-bold">
+            See My Work
+          </button>
+          <button className="bg-blue-500 text-white hover:bg-blue-200 ease-out duration-200 p-4 rounded-lg font-bold">
+            Contact Me
+          </button>
         </div>
       </motion.div>
       <div>
