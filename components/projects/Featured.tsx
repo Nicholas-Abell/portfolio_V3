d5d9e2708be507231bf4b866552475d11b1d36c5
Nick <darkforceyoda@gmail.com>
2023-12-03 15:31:53 -0600

    video now plays when in view

--- components/projects/Featured.tsx
+++ components/projects/Featured.tsx
@@ -1,9 +1,10 @@
 "use client";
 import Link from "next/link";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { CgWebsite } from "react-icons/cg";
 import { FaGithub } from "react-icons/fa";
 import FramerMotionWrapper from "../shared/FramerMotionWrapper";
+import { inView, useInView } from "framer-motion";
 
 type FeaturedProps = {
   title: string;
@@ -24,28 +25,38 @@ const Featured: React.FC<FeaturedProps> = ({
   fromRight,
   video,
 }) => {
-  const [showVideo, setShowVideo] = useState(false);
   const videoRef = useRef<HTMLVideoElement | null>(null);
-  const handleHover = () => {
-    videoRef.current?.play();
-    setShowVideo(true);
-  };
+  const isInView = useInView(videoRef);
 
-  const handleLeave = () => {
-    videoRef.current?.pause();
-    setShowVideo(false);
-  };
+  const [showVideo, setShowVideo] = useState(false);
+
+  useEffect(() => {
+    if (isInView) {
+      setShowVideo(true);
+      videoRef.current?.play();
+    } else {
+      setShowVideo(false);
+      videoRef.current?.pause();
+    }
+  }, [isInView]);
+
   return (
     <div
-      onMouseEnter={handleHover}
-      onMouseLeave={handleLeave}
-      className={`w-full flex flex-col-reverse  text- rounded-lg ${
+      className={`w-full flex flex-col gap-4 lg:gap-0 lg:min-h-screen text- rounded-lg ${
         fromRight ? "lg:flex-row bg-pink-50" : "lg:flex-row-reverse"
       } items-center justify-center px-4 py-4 lg:py-12`}
     >
-      <div className="hidden md:block w-full relative border-2 border-black">
+      <FramerMotionWrapper
+        variant={fromRight ? "fromRight" : "fromLeft"}
+        className="py-2 lg:hidden"
+      >
+        <p className={`${fromRight && "text-end"} pb-2`}>Featured Project</p>
+        <h3 className={`${fromRight && "text-end"} text-5xl`}>{title}</h3>
+      </FramerMotionWrapper>
+
+      <div className="w-full relative border-2 border-black">
         <div
-          className={`absolute h-full top-0 left-0 bg-gray-600/60 z-10 ease-in-out duration-200 ${
+          className={`absolute h-full top-0 left-0 bg-gray-600/60 z-10 ease-in-out duration-1000 ${
             showVideo ? "w-0" : "w-full"
           }`}
         />
@@ -61,30 +72,15 @@ const Featured: React.FC<FeaturedProps> = ({
           fromRight ? "lg:ml-[-120px]" : "lg:mr-[-120px]"
         } flex flex-col lg:justify-between z-10`}
       >
-        <div>
-          <FramerMotionWrapper
-            variant={fromRight ? "fromRight" : "fromLeft"}
-            className="py-2"
-          >
-            <p className={`${fromRight && "text-end"} pb-2`}>
-              Featured Project
-            </p>
-            <h3 className={`${fromRight && "text-end"} text-5xl`}>{title}</h3>
-          </FramerMotionWrapper>
-        </div>
-        <div className="md:hidden block w-full relative border-2 border-black">
-          <div
-            className={`absolute h-full top-0 left-0 bg-gray-600/60 z-10 ease-in-out duration-200 ${
-              showVideo ? "w-0" : "w-full"
-            }`}
-          />
-          <video ref={videoRef} muted loop>
-            {/* <source src="/video-example.webm" type="video/webm" /> */}
-            <source src={video} type="video/mp4" />
-            {"Sorry, your browser doesn't support videos."}
-          </video>
-        </div>
-        <div className="bg-[#dbecfd] py-4 px-4 lg:px-8 rounded-lg z-0">
+        <FramerMotionWrapper
+          variant={fromRight ? "fromRight" : "fromLeft"}
+          className="py-2 hidden lg:block"
+        >
+          <p className={`${fromRight && "text-end"} pb-2`}>Featured Project</p>
+          <h3 className={`${fromRight && "text-end"} text-5xl`}>{title}</h3>
+        </FramerMotionWrapper>
+
+        <div className="bg-[#dbecfd] py-4 px-4 lg:px-8 rounded-lg z-0 shadow-md shadow-purple-800">
           <p>{description}</p>
           <div className="w-full">
             <ul className="flex items-center justify-center gap-4 pt-4">
@@ -94,6 +90,7 @@ const Featured: React.FC<FeaturedProps> = ({
             </ul>
           </div>
         </div>
+
         <div
           className={`flex items-center gap-4 pb-4 px-4 ${
             fromRight ? "justify-end" : "justify-start"
