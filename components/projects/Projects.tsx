07644add2e713cfcb609b1607280fadbc79c0778
Nick <darkforceyoda@gmail.com>
2023-12-03 10:48:37 -0600

    improvement to mobile layout

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -2,21 +2,13 @@
 import React, { useRef } from "react";
 import FramerMotionWrapper from "../shared/FramerMotionWrapper";
 import Featured from "./Featured";
+import Image from "next/image";
 
 type ProjectsProps = {};
 
 const Projects: React.FC<ProjectsProps> = () => {
-  const videoRef = useRef<HTMLVideoElement | null>(null);
-  const handleHover = () => {
-    videoRef.current?.play();
-  };
-
-  const handleLeave = () => {
-    videoRef.current?.pause();
-  };
-
   return (
-    <section className="w-full min-h-screen flex flex-col justify-center items-center gap-8 overflow-hidden mx-auto px-2.5 lg:px-20 py-24 relative bg-[#f5f5f3]">
+    <section className="w-full min-h-screen flex flex-col justify-center items-center gap-8 overflow-hidden mx-auto py-24 relative">
       <FramerMotionWrapper variant="fromBottom">
         <h2 className="text-center text-4xl md:text-6xl py-8">
           Personal Projects
@@ -48,7 +40,7 @@ const Projects: React.FC<ProjectsProps> = () => {
         title="Reddit Clone"
         githubLink="/"
         liveLink="/"
-        description="   A full stack dating application implementing Next Js server
+        description="A full stack dating application implementing Next Js server
         actions. Features include: user registration, profile Management,
         messaging, and filtering based on user preferences."
         skills={["ReactJs", "Firebase", "Tailwind"]}
@@ -64,8 +56,28 @@ const Projects: React.FC<ProjectsProps> = () => {
         </FramerMotionWrapper>
 
         <FramerMotionWrapper variant="fromBottom">
-          <div className="w-full grid grid-cols-2 lg:grid-cols-3 gap-4 py-4 lg:px-24">
-            <div className="bg-gray-400 h-[320px] rounded-lg" />
+          <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-4 py-4 px-4 lg:px-16  xl:px-24 overflow-hidden">
+            <div className=" bg-gradient-to-r from-red-400 pt-4 pl-4 rounded-2xl">
+              <div className="h-[320px] rounded-lg relative">
+                <div className="absolute top-0 left-0 z-10 w-0 hover:w-full ease-in duration-200 h-full bg-black/70" />
+                <Image
+                  src="/imageEverdell.png"
+                  alt="everdell"
+                  fill
+                  className="rounded-lg absolute left-2"
+                />
+              </div>
+            </div>
+            <div className=" bg-gradient-to-r from-blue-200 pt-4 pl-4 rounded-2xl">
+              <div className="h-[320px] rounded-lg relative">
+                <Image
+                  src="/imageThistle.png"
+                  alt="everdell"
+                  fill
+                  className="rounded-lg absolute left-2"
+                />
+              </div>
+            </div>
             <div className="bg-gray-400 h-[320px] rounded-lg" />
             <div className="bg-gray-400 h-[320px] rounded-lg" />
             <div className="bg-gray-400 h-[320px] rounded-lg" />
