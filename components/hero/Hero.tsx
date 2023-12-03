d498722bd96e49431dc700bdb303ef30d2f012b1
Nick <darkforceyoda@gmail.com>
2023-12-03 12:27:28 -0600

    .

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import Image from "next/image";
 import profileImage from "../../public/profile.png";
 import { motion } from "framer-motion";
+import FramerMotionWrapper from "../shared/FramerMotionWrapper";
 
 type HeroProps = {};
 
 const Hero: React.FC<HeroProps> = () => {
   return (
     <section className="w-full min-h-screen grid md:grid-cols-2 items-center gap-8 mx-auto px-2.5 md:px-48 pt-8 relative overflow-hidden">
-      <div className="bg-[#f5eef0] w-[1600px] h-[900px] absolute bottom-60 right-[-360px] rotate-45" />
+      <div className="bg-[#d1defa] w-[1600px] h-[900px] absolute bottom-60 right-[-360px] rotate-45" />
       <motion.div
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
@@ -31,14 +32,16 @@ const Hero: React.FC<HeroProps> = () => {
         </div>
       </motion.div>
       <div>
-        <div className="relative w-[320px] md:h-[480px] h-[320px] md:w-[480px]">
-          <Image
-            src={profileImage}
-            alt="profileImage"
-            fill
-            className="object-fill rounded-full"
-          />
-        </div>
+        <FramerMotionWrapper variant="fromRight">
+          <div className="relative w-[320px] md:h-[480px] h-[320px] md:w-[480px]">
+            <Image
+              src={profileImage}
+              alt="profileImage"
+              fill
+              className="object-fill rounded-full border-8 border-white"
+            />
+          </div>
+        </FramerMotionWrapper>
       </div>
     </section>
   );
