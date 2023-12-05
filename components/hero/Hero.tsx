832a047cf73b2d32231ceaa8854ab575df2ea5d6
Nick <darkforceyoda@gmail.com>
2023-12-05 16:56:41 -0600

    hero layout for title/ animations

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,47 +1,69 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import profileImage from "../../public/profile.png";
 import { motion } from "framer-motion";
 import FramerMotionWrapper from "../shared/FramerMotionWrapper";
+import { SiGithub, SiGmail, SiLinkedin } from "react-icons/si";
+import Link from "next/link";
 
 type HeroProps = {};
 
 const Hero: React.FC<HeroProps> = () => {
   return (
-    <section className="w-full min-h-screen grid md:grid-cols-2 items-center gap-8 mx-auto px-2.5 md:px-48 pt-8 relative overflow-hidden">
-      <div className="bg-[#d1defa] w-[1600px] h-[900px] absolute bottom-60 right-[-360px] rotate-45" />
-      <motion.div
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.5 }}
-        className="flex flex-col justify-center gap-8"
-      >
-        <p>Hello There, I am</p>
-        <h1 className="text-4xl md:text-6xl font-bold">Nicholas Abell</h1>
-        <p>
-          I am passionate about solving bussiness problems through innovation
-        </p>
-        <div className="grid grid-cols-2 items-center gap-4 pr-24">
-          <button className="bg-[#f1cbd6] hover:bg-[#f5eef0] ease-out duration-200 p-4 rounded-lg font-bold">
-            See My Work
-          </button>
-          <button className="bg-blue-500 text-white hover:bg-blue-200 ease-out duration-200 p-4 rounded-lg font-bold">
-            Contact Me
-          </button>
+    <section className="w-full h-screen pt-20 grid grid-rows-3">
+      <div className="grid grid-cols-4 gap-4 row-span-1">
+        <FramerMotionWrapper
+          variant="fromLeft"
+          className="test flex items-center py-12 text-sm text-gray-700 col-span-1 px-4"
+        >
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam
+            placeat enim accusamus voluptate eveniet libero excepturi pariatur
+          </p>
+        </FramerMotionWrapper>
+        <div className="test flex justify-end items-center pt-12 col-span-2">
+          <div className="w-full mx-auto flex flex-col justify-center items-center">
+            <p>Software Developer</p>
+            <h1 className="text-4xl lg:text-5xl font-bold">Nicholas Abell</h1>
+          </div>
         </div>
-      </motion.div>
-      <div>
-        <FramerMotionWrapper variant="fromRight">
-          <div className="relative w-[320px] md:h-[480px] h-[320px] md:w-[480px]">
+        <FramerMotionWrapper
+          variant="fromRight"
+          className="flex items-start justify-center test pt-12 col-span-1"
+        >
+          <div className="flex items-center gap-8">
             <Image
-              src={profileImage}
-              alt="profileImage"
-              fill
-              className="object-fill rounded-full border-8 border-white"
+              width={180}
+              height={120}
+              src="/profile.png"
+              alt="profile"
+              className="rounded-full shadow-lg shadow-gray-400"
             />
+            <div className="grid grid-cols-1 gap-4 text-gray-400">
+              <Link
+                className="hover:text-black rounded-full shadow-md shadow-gray-400 hover:shadow-purple-300 duration-200 ease-in-out p-2 flex justify-center items-center"
+                href="/"
+              >
+                <SiGithub size={15} />
+              </Link>
+              <Link
+                className="hover:text-black rounded-full shadow-md shadow-gray-400 hover:shadow-purple-300 duration-200 ease-in-out p-2 flex justify-center items-center"
+                href="/"
+              >
+                <SiLinkedin size={15} />
+              </Link>
+              <Link
+                className="hover:text-black rounded-full shadow-md shadow-gray-400 hover:shadow-purple-300 duration-200 ease-in-out p-2 flex justify-center items-center"
+                href="/"
+              >
+                <SiGmail size={15} />
+              </Link>
+            </div>
           </div>
         </FramerMotionWrapper>
+      </div>
+      <div className="bg-blue-200 row-span-2 test">
+        <p>d</p>
       </div>
     </section>
   );
