4977c41256696d53a2cff05eff359747ee3cfbb3
Nick <darkforceyoda@gmail.com>
2023-12-06 11:15:56 -0600

    hero layout

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import FramerMotionWrapper from "../shared/FramerMotionWrapper";
 import { SiGithub, SiGmail, SiLinkedin } from "react-icons/si";
+import { FaDesktop, FaMobileScreen } from "react-icons/fa6";
 import Link from "next/link";
 
 type HeroProps = {};
@@ -14,14 +15,14 @@ const Hero: React.FC<HeroProps> = () => {
       <div className="grid grid-cols-4 gap-4 row-span-1">
         <FramerMotionWrapper
           variant="fromLeft"
-          className="test flex items-center py-12 text-sm text-gray-700 col-span-1 px-4"
+          className="flex items-center py-12 text-xs lg:text-sm text-gray-700 col-span-1 px-4"
         >
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam
             placeat enim accusamus voluptate eveniet libero excepturi pariatur
           </p>
         </FramerMotionWrapper>
-        <div className="test flex justify-end items-center pt-12 col-span-2">
+        <div className="flex justify-end items-center pt-12 col-span-2">
           <div className="w-full mx-auto flex flex-col justify-center items-center">
             <p>Software Developer</p>
             <h1 className="text-4xl lg:text-5xl font-bold">Nicholas Abell</h1>
@@ -29,9 +30,9 @@ const Hero: React.FC<HeroProps> = () => {
         </div>
         <FramerMotionWrapper
           variant="fromRight"
-          className="flex items-start justify-center test pt-12 col-span-1"
+          className="flex items-start justify-center pt-12 col-span-1"
         >
-          <div className="flex items-center gap-8">
+          <div className="flex flex-col lg:flex-row items-center gap-2 lg:gap-8">
             <Image
               width={180}
               height={120}
@@ -39,7 +40,7 @@ const Hero: React.FC<HeroProps> = () => {
               alt="profile"
               className="rounded-full shadow-lg shadow-gray-400"
             />
-            <div className="grid grid-cols-1 gap-4 text-gray-400">
+            <div className="grid grid-cols-3 lg:grid-cols-1 gap-2 lg:gap-4 text-gray-400">
               <Link
                 className="hover:text-black rounded-full shadow-md shadow-gray-400 hover:shadow-purple-300 duration-200 ease-in-out p-2 flex justify-center items-center"
                 href="/"
@@ -62,8 +63,26 @@ const Hero: React.FC<HeroProps> = () => {
           </div>
         </FramerMotionWrapper>
       </div>
-      <div className="bg-blue-200 row-span-2 test">
-        <p>d</p>
+      <div className="row-span-1"></div>
+      <div className="row-span-1 relative">
+        <div className="grid grid-cols-3 grid-rows-1 justify-center h-full">
+          <div className="w-full flex justify-center items-center">
+            <div className=" rounded-full bg-white border-4 border-[#f5eef0] p-4">
+              <FaMobileScreen size={50} />
+            </div>
+          </div>
+          <div className="w-full flex justify-center items-center">
+            <div className=" rounded-full bg-white border-4 border-[#f5eef0] p-4">
+              <FaDesktop size={50} />
+            </div>
+          </div>
+          <div className="w-full flex justify-center items-center">
+            <div className=" rounded-full bg-white border-4 border-[#f5eef0] p-4">
+              <FaMobileScreen size={50} />
+            </div>
+          </div>
+        </div>
+        <div className="absolute z-[-10] w-full h-[50%] bottom-0 left-0 bg-[#f5eef0]" />
       </div>
     </section>
   );
