b95bef3751c1dd236a650b7d632646e737e8aae0
Nick <darkforceyoda@gmail.com>
2023-12-08 17:08:20 -0600

    loading animation

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -11,12 +11,12 @@ const Hero: React.FC<HeroProps> = () => {
   return (
     <section className="w-full min-h-screen pt-20 bg-light text-light grid grid-cols-3 relative">
       <div className="grid grid-cols-3 col-span-3 sm:col-span-2 grid-rows-5 p-4 pb-0 sm:pb-4 sm:pr-2 gap-2 sm:gap-4">
-        <div className="relative bg-gradient-to-r from-dark to-darker rounded-lg col-span-3 row-span-4 text-light p-4 md:p-12 flex flex-col justify-between">
+        <div className="relative bg-gradient-to-r from-dark to-darker rounded-lg col-span-3 row-span-4 text-light p-4 md:p-12 flex flex-col justify-between z-20">
           <div>
             <p className="text-gray-300 text-xl lg:text-4xl font-bold">
               Software Developer
             </p>
-            <h1 className="pt-8 md:pt-4 text-4xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-9xl font-bold text-main">
+            <h1 className="text-4xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-9xl font-bold text-main">
               NICHOLAS <br className="hidden sm:block" /> ABELL
             </h1>
           </div>
@@ -27,12 +27,24 @@ const Hero: React.FC<HeroProps> = () => {
             </p>
           </div>
         </div>
-        <div className="bg-main rounded-lg row-span-2" />
-        <div className="rounded-lg bg-darkAccent col-span-2 row-span-2" />
+        <motion.div
+          initial={{ y: -200 }}
+          animate={{ y: 0 }}
+          transition={{ type: "spring", stiffness: 50 }}
+          className="row-span-4 col-span-3 grid grid-cols-3 gap-4"
+        >
+          <motion.div
+            initial={{ x: 400 }}
+            animate={{ x: 0 }}
+            transition={{ type: "spring", stiffness: 60, delay: 1 }}
+            className="bg-main rounded-lg row-span-2"
+          />
+          <div className="rounded-lg bg-darkAccent col-span-2 row-span-2 z-10" />
+        </motion.div>
       </div>
       <div className="h-full grid-cols-3 grid-rows-6 grid gap-2 p-4 pt-2 sm:pt-4 pl-2 col-span-3 sm:col-span-1">
-        <div className="bg-gradient-to-r from-dark to-darker rounded-xl col-span-2 sm:col-span-3 relative flex justify-center items-center row-span-3">
-          <div className="relative w-[160px] h-[160px] lg:w-[240px] lg:h-[240px]">
+        <div className="bg-gradient-to-r from-dark to-darker rounded-xl col-span-2 sm:col-span-3 relative flex justify-center items-center row-span-3 z-10 py-4">
+          <div className="relative w-[120px] h-[120px] sm:w-[160px] sm:h-[160px] lg:w-[240px] lg:h-[240px]">
             <Image
               src="/profile.png"
               alt="profile"
@@ -44,14 +56,16 @@ const Hero: React.FC<HeroProps> = () => {
         <div className="bg-accent row-span-3 rounded-lg block sm:hidden" />
         <Link
           href="https://github.com/Nicholas-Abell"
-          className="row-span-2 sm:row-span-1"
+          className="row-span-2 sm:row-span-1 z-[2]"
         >
           <motion.div
             whileHover={{
-              scale: 1.1,
               rotate: [-5, 5, -5, 5, 0],
               transition: { duration: 0.3 },
             }}
+            initial={{ y: -150 }}
+            animate={{ y: 0 }}
+            transition={{ type: "spring", stiffness: 50 }}
             className="bg-main w-full h-full rounded-lg flex
             justify-center items-center"
           >
@@ -60,13 +74,18 @@ const Hero: React.FC<HeroProps> = () => {
         </Link>
         <Link
           href="https://www.linkedin.com/in/nicholas-abell-348807192/"
-          className="row-span-2 sm:row-span-1"
+          className="row-span-2 sm:row-span-1 z-[1]"
         >
           <motion.div
             whileHover={{
-              scale: 1.1,
               rotate: [5, -5, 5, -5, 0],
               transition: { duration: 0.3 },
+            }}
+            initial={{ x: -180, y: -150 }}
+            animate={{ x: 0, y: 0 }}
+            transition={{
+              y: { type: "spring", stiffness: 60, delay: 0 },
+              x: { type: "spring", stiffness: 60, delay: 1 },
             }}
             className="bg-accent rounded-lg flex justify-center items-center w-full h-full"
           >
@@ -79,17 +98,19 @@ const Hero: React.FC<HeroProps> = () => {
         >
           <motion.div
             whileHover={{
-              scale: 1.1,
               rotate: [-5, 5, -5, 5, 0],
               transition: { duration: 0.3 },
             }}
+            initial={{ y: 150 }}
+            animate={{ y: 0 }}
+            transition={{ type: "spring", stiffness: 50, delay: 1 }}
             className="bg-darkAccent w-full h-full rounded-lg flex
-            justify-center items-center"
+            justify-center items-center z-[1]"
           >
             <SiGmail size={45} className="text-light" />
           </motion.div>
         </Link>
-        <div className="rounded-xl col-span-3 bg-gradient-to-r from-dark to-darker row-span-2" />
+        <div className="rounded-xl col-span-3 bg-gradient-to-r from-dark to-darker row-span-2 z-10" />
       </div>
     </section>
   );
