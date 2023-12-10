1e2e4748d8b8567174d6ad088541f38e0e7e098c
Nick <darkforceyoda@gmail.com>
2023-12-10 00:04:32 -0600

    mobile style improvements

--- components/testimonials/Card.tsx
+++ components/testimonials/Card.tsx
@@ -28,10 +28,10 @@ const Card: React.FC<CardProps> = ({
         className=" rounded-full translate-y-1/2 border-8 border-light border-solid"
         alt="a"
       />
-      <div className="w-full bg-light p-4 rounded-lg flex flex-col items-center justify-between gap-8 pt-24 h-full">
+      <div className="w-full bg-light p-4 rounded-lg flex flex-col items-center justify-between gap-2 md:gap-8 pt-24 h-full">
         <div className="flex flex-col items-center gap-2">
           <ImQuotesLeft size={25} className="text-main" />
-          <p className="text-gray-600 text-xl px-12">{content}</p>
+          <p className="text-gray-600 text-xl px-4 md:px-12">{content}</p>
         </div>
         <div>
           <p className="text-center py-8 font-bold">{name}</p>
@@ -40,7 +40,7 @@ const Card: React.FC<CardProps> = ({
             href={link}
             className={`${
               siteTitle ? " visible" : " invisible"
-            } flex items-center gap-2 text-gray-400`}
+            } flex items-center gap-2 text-gray-400 whitespace-nowrap`}
           >
             {siteTitle}
             <FaLink />
--- components/testimonials/Testimonials.tsx
+++ components/testimonials/Testimonials.tsx
@@ -17,7 +17,7 @@ const Testimonials: React.FC<TestimonialsProps> = () => {
         </div>
       </FramerMotionWrapper>
       <FramerMotionWrapper variant="fromBottom">
-        <div className="grid lg:grid-cols-3 py-4 md:gap-4 px-8 md:px-0 grid-rows-1">
+        <div className="grid xl:grid-cols-3 py-4 md:gap-4 md:px-0 grid-rows-1">
           <Card
             name="Mary"
             link="thistleandthornsllc.com"
