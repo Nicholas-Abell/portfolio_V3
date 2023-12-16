4c71bfacccaebe7c97124ace3538197a4ff2c261
Nick <darkforceyoda@gmail.com>
2023-12-15 23:46:39 -0600

    projects link animation

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -11,10 +11,10 @@ const Hero: React.FC<HeroProps> = () => {
   const [loading, setLoading] = useState(true);
 
   return (
-    <section className="w-full min-h-screen bg-light text-light grid grid-cols-3 relative">
+    <section className="w-full min-h-screen bg-light text-light grid grid-cols-3 relative overflow-hidden">
       <div className="grid grid-cols-3 col-span-3 sm:col-span-2 grid-rows-5 p-4 pb-0 sm:pb-4 sm:pr-2 gap-2 sm:gap-4">
         <div className="relative rounded-lg col-span-3 row-span-4 text-light grid grid-cols-3 grid-rows-3 z-20 gap-4">
-          <div className=" col-span-3 row-span-2 flex items-center justify-center text-center bg-gradient-to-r from-dark to-darker rounded-br-lg">
+          <div className=" col-span-3 row-span-2 flex items-center justify-center text-center bg-gradient-to-r from-dark to-darker rounded-br-lg rounded-bl-lg md:rounded-bl-none z-20">
             <div className="text-left">
               <p>Software Developer</p>
               <h1 className="text-4xl md:text-6xl xl:text-7xl font-bold text-light">
@@ -25,7 +25,10 @@ const Hero: React.FC<HeroProps> = () => {
           <motion.div
             whileHover={{ y: "-5%", transition: { delay: 0 } }}
             whileTap={{ scale: 0.9, transition: { delay: 0 } }}
-            className="rounded-lg text-xl font-bold bg-accent col-span-3 row-span-2 z-10 border shadow-md shadow-gray-400"
+            initial={{ y: "-200%" }}
+            animate={{ y: 0 }}
+            transition={{ type: "spring", stiffness: 60 }}
+            className="rounded-lg text-xl font-bold bg-accent col-span-3 row-span-2 border shadow-md shadow-gray-400 z-10"
           >
             <Link
               href="/#projects"
@@ -84,7 +87,12 @@ const Hero: React.FC<HeroProps> = () => {
             />
           </div>
         </div>
-        <div className="bg-accent row-span-3 rounded-lg block sm:hidden z-10 border shadow-md shadow-gray-400" />
+        <motion.div
+          initial={{ x: "150%" }}
+          animate={{ x: 0, transition: { delay: 1.5 } }}
+          transition={{ type: "spring", stiffness: 60 }}
+          className="bg-accent row-span-3 rounded-lg block sm:hidden z-10 border shadow-md shadow-gray-400"
+        />
         <Link
           href="https://github.com/Nicholas-Abell"
           className="row-span-2 sm:row-span-1 z-[2]"
@@ -97,7 +105,7 @@ const Hero: React.FC<HeroProps> = () => {
             }}
             initial={{ y: "-115%" }}
             animate={{ y: 0 }}
-            transition={{ type: "spring", stiffness: 50 }}
+            transition={{ type: "spring", stiffness: 60 }}
             className="bg-main w-full h-full rounded-lg flex
             justify-center items-center border shadow-md shadow-gray-400"
           >
@@ -135,8 +143,8 @@ const Hero: React.FC<HeroProps> = () => {
               rotate: [-5, 5, -5, 5, 0],
               transition: { duration: 0.3 },
             }}
-            initial={{ y: "-115%" }}
-            animate={{ y: 0 }}
+            initial={{ x: "150%" }}
+            animate={{ x: 0 }}
             transition={{ type: "spring", stiffness: 50, delay: 1 }}
             className="bg-darkAccent w-full h-full rounded-lg flex
             justify-center items-center z-[1] border shadow-md shadow-gray-400"
